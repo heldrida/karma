72de85d97698acae0f5ffb0021e1cf7a134cd47e
heldrida <heldrida@gmail.com>
2014-07-06 15:37:22 +0000

    removed bootstrap from bower json. created mouse wheel function, that animates to next and previous panels.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
+"use strict";
 
-	console.log("DOMContentLoaded!");
+document.addEventListener('DOMContentLoaded', function () {
 
-	window.addEventListener('mousewheel', function() {
-	        console.log('mouse wheel!');
+	var wrapper = document.getElementById('wrapper');
+	var delve = document.getElementById('delve');
+
+	delve.addEventListener('click', function(){
+		wrapper.style.top = '-100%';
+	});
+
+	var mw_lock = false;
+
+	["transitionend", "webkitTransitionEnd", "mozTransitionEnd"].forEach(function(transition) {
+
+		wrapper.addEventListener(transition, function(e){
+			mw_lock = false;
+		}, false);
+
+	});
+
+	window.addEventListener('mousewheel', function(e) {
+
+		(function(){
+
+			var y = parseFloat(wrapper.style.top) || 0;
+
+			if (mw_lock === false){
+
+				mw_lock = true;
+
+				if (y <= 0 && !( y == 0 && e.wheelDelta > 0) && !( y <= -200 && e.wheelDelta < 0)){
+
+					wrapper.style.top = e.wheelDelta > -1 ? (y + 100) + '%' : (y - 100) + '%';
+
+				} else {
+
+					mw_lock = false;
+
+				}
+
+			}
+
+		}());
+
+
 	});
 
 }, false);
